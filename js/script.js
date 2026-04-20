020acfbdd38912e8bf7bb7504e21e36fd697c61d
laripinheiroz <larissapinnheiroz@gmail.com>
2026-04-20 13:26:14 -0300

    adiciona js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// ===== REVEAL =====
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+}
+
+revealOnScroll();
+window.addEventListener('scroll', revealOnScroll);
